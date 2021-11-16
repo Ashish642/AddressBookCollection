93d73ccbbbe1d9f470ac749349d2c78045f966b0
pitu <ashishkumarvish.432@gmail.com>
2021-11-16 15:06:03 +0530

    Added to there is no duplicate entry for same

---
+++ src/com/bridgelabz/AddressBookInfo.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+
+// interface
+public interface AddressBookInfo {
+    public void add();
+
+    public void edit(String firstName);
+
+    public void delete(String firstName);
+
+    public void sortAlphabetically();
+}
--- src/com/bridgelabz/AddressBookSystem.java
+++ src/com/bridgelabz/AddressBookSystem.java
@@ -1,75 +1,82 @@
 package com.bridgelabz;
-import  java.util.ArrayList;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class AddressBookSystem  implements  Info {
-    // ArrayList Declare
+//AddressBook implements AddressBookInfo
+public class AddressBookSystem implements AddressBookInfo {
+    // Declaring ArrayList
     ArrayList<Person> book = new ArrayList<>();
-    // Creating scanner class object
-    Scanner scanner = new Scanner(System.in);
-    public void add() {
-        // user input to print the details
-        System.out.println("Enter rhe Firstname: ");
-        String firstName = scanner.nextLine();
-        System.out.println("Enter the Lastname");
-        String lastName = scanner.nextLine();
+    // Creating scanner object
+    Scanner scan = new Scanner(System.in);
 
+    @Override
+    public void add() {
+        System.out.println("Enter the AddressBook Name");
+        scan.nextLine();
+        System.out.println("Enter the Person Firstname: ");
+        String firstName = scan.nextLine();
+        System.out.println("Enter the Person Lastname: ");
+        String lastName = scan.nextLine();
 
         for (Person value : book) {
             if (value.getFirstName().equalsIgnoreCase(firstName)) {
                 if (value.getLastName().equalsIgnoreCase(lastName)) {
-                    System.out.println("Name is already exist please update/edit your details with option 2");
+                    System.out.println("Sorry Name is already exist please update/edit your details with option 2");
                     return;
                 }
 
             }
-
         }
-
-            System.out.println("Enter the Address: ");
-        String address = scanner.nextLine();
-        System.out.println("Enter the City :");
-        String city = scanner.nextLine();
-        System.out.println("Enter the PhoneNumber: ");
-        long phoneNumber = scanner.nextLong();
-        System.out.println("Enter the State: ");
-        String state= scanner.nextLine();
+        System.out.println("Enter the Person Address: ");
+        String address = scan.nextLine();
+        System.out.println("Enter the Person City: ");
+        String city = scan.nextLine();
+        System.out.println("Enter the Person State: ");
+        String state = scan.nextLine();
+        System.out.println("Enter the Person Phone Number: ");
+        long phoneNumber = scan.nextLong();
         System.out.println("Enter the Zip code: ");
-        String zip = scanner.nextLine();
-        Person person = new Person(firstName,lastName,address,city,state,phoneNumber,zip);
+        scan.nextLine();
+        String zip = scan.nextLine();
+        Person person = new Person(firstName, lastName, address, city, state, phoneNumber, zip);
         book.add(person);
-        System.out.println("Successfully Add...");
+        System.out.println("Successfully Added!!");
     }
-    /* Method to edit or update the details using firstname */
 
+    /* Method to edit or update the details using firstname */
+    @Override
     public void edit(String firstName) {
         for (Person value : book) {
 
             if (value.getFirstName().equalsIgnoreCase(firstName)) {
-                System.out.println("Hey  " + value.getFirstName() + " Please edit your details");
-                System.out.println("Hey " + value.getFirstName() + " Please edit your address");
-                scanner.next();
-                String address = scanner.nextLine();
-                value.setAddress(address);
-                System.out.println("Hey  " + value.getFirstName() + " Please edit your city");
-                String city = scanner.next();
-                value.setCity(city);
-                System.out.println("Hey " + value.getFirstName() + " Please edit your state");
-                String state = scanner.next();
-                value.setState(state);
-                System.out.println("Hey " + value.getFirstName() + " Please edit your phone number");
-                long phone = scanner.nextLong();
-                value.setPhoneNumber(phone);
-                System.out.println("Hey " + value.getFirstName() + " Please edit your zip");
-                scanner.nextLine();
-                String zip = scanner.nextLine();
-                value.setZip(zip);
-                System.out.println("Hey " + value.getFirstName() + " Successfully you have updated your details. ");
+                Person person = value;
+                System.out.println("Hi  " + person.getFirstName() + " Please edit your details");
+                System.out.println("Hi " + person.getFirstName() + " Please edit your address");
+                scan.next();
+                String address = scan.nextLine();
+                person.setAddress(address);
+                System.out.println("Hi  " + person.getFirstName() + " Please edit your city");
+                String city = scan.next();
+                person.setCity(city);
+                System.out.println("Hi " + person.getFirstName() + " Please edit your state");
+                String state = scan.next();
+                person.setState(state);
+                System.out.println("Hi " + person.getFirstName() + " Please edit your phone number");
+                long phone = scan.nextLong();
+                person.setPhoneNumber(phone);
+                System.out.println("Hi " + person.getFirstName() + " Please edit your zip");
+                scan.nextLine();
+                String zip = scan.nextLine();
+                person.setZip(zip);
+                System.out.println("Hi " + person.getFirstName() + " Successfully you have updated your details. ");
             }
         }
 
     }
     /* Method to delete the details using firstname */
+
+    @Override
     public void delete(String firstName) {
         for (int select = 0; select < book.size(); select++) {
             if (book.get(select).getFirstName().equalsIgnoreCase(firstName)) {
@@ -78,14 +85,22 @@ public class AddressBookSystem  implements  Info {
                 System.out.println("Successfully Deleted!");
             }
         }
+
     }
 
+    @Override
+    public void sortAlphabetically() {
+        book.stream().sorted((p1, p2) -> p1.getFirstName().compareToIgnoreCase(p2.getFirstName()))
+                .forEach(System.out::println);
 
+        // Collections.sort(book, (p1, p2) ->
+        // p1.getFirstName().compareTo(p2.getFirstName()));
+        // System.out.println(book);
+    }
 
-        // Display the person details added
+    // Display the person details added
     public void display() {
         for (Person person : book)
             System.out.println(person);
     }
 }
-
